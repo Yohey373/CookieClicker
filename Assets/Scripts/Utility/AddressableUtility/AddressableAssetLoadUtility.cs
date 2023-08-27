cf6818920b83a9c4bb637e8afdd1b02cfacee90b
yohey373 <load5123874@gmail.com>
2023-08-27 11:22:41 +0900

    AssetBundleの取得先をAWSに変更

--- Assets/Scripts/Utility/AddressableUtility/AddressableAssetLoadUtility.cs
+++ Assets/Scripts/Utility/AddressableUtility/AddressableAssetLoadUtility.cs
@@ -2,14 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class AddressableAssetLoadUtility : SingletonMonoBehaviour<AddressableAssetLoadUtility>
 {
+    private AsyncOperationHandle assetOperation = new AsyncOperationHandle();
+
     public T LoadAssetAsync<T>(string address) where T : Object
     {
-        var assetOperation = Addressables.LoadAssetAsync<T>(address);
+        assetOperation = Addressables.LoadAssetAsync<T>(address);
         var asset = assetOperation.WaitForCompletion();
-        Addressables.Release(assetOperation);
-        return asset;
+        return (T)asset;
+    }
+
+    private void OnDestroy()
+    {
+        if(assetOperation.IsValid())
+        {
+            Addressables.Release(assetOperation);
+        }
     }
 }
